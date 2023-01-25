a7537f405216be6fc3f6d7347f2bc8e11ea36d3d
Sunbearian <stigcollins@icloud.com>
2023-01-25 13:48:06 +0000

    fecth request working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./App.css";
+import CardUpdate from "./fetch.js";
 
 function App() {
 	return (
 		<ChakraProvider>
 			<div>Hello World</div>
+			<CardUpdate />
 		</ChakraProvider>
 	);
 }
