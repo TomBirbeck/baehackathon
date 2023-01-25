503d52524d84120176704701f777f12b33f53669
Sunbearian <stigcollins@icloud.com>
2023-01-25 14:24:53 +0000

    Add fetch to card

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
-import { Container } from '@chakra-ui/react';
-import './App.css';
-import CardGrid from './components/CardGrid';
-import theme from './theme';
-import TotalCO2 from './components/TotalCO2';
-import CardUpdate from './fetch.js';
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Container } from "@chakra-ui/react";
+import "./App.css";
+import CardGrid from "./components/CardGrid";
+import theme from "./theme";
+import TotalCO2 from "./components/TotalCO2";
 
 function App() {
-  return (
-    <ChakraProvider theme={theme}>
-      <Container w={'90vw'} paddingBlockEnd={10}>
-        <TotalCO2 />
-        <CardGrid />
-      </Container>
-      <CardUpdate />
-    </ChakraProvider>
-  );
+	return (
+		<ChakraProvider theme={theme}>
+			<Container w={"90vw"} paddingBlockEnd={10}>
+				<TotalCO2 />
+				<CardGrid />
+			</Container>
+		</ChakraProvider>
+	);
 }
 
 export default App;
