9d230d45dc9b46341eef569c4605133ccecf88cb
TomBirbeck <tombirbeck47@gmail.com>
2023-01-25 13:55:39 +0000

    fixed error

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
-import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
-import { Container } from "@chakra-ui/react";
-import "./App.css";
-
-import CardUpdate from "./fetch.js";
-
-function App() {
-	return (
-		<ChakraProvider>
-			<div>Hello World</div>
-			
-=======
-import CardGrid from "./components/CardGrid";
-import theme from "./theme";
-import TotalCO2 from "./components/TotalCO2";
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
+import './App.css';
+import CardGrid from './components/CardGrid';
+import theme from './theme';
+import TotalCO2 from './components/TotalCO2';
+import CardUpdate from './fetch.js';
 
 function App() {
-	return (
-		<ChakraProvider theme={theme}>
-			<Container w={"90vw"} paddingBlockEnd={10}>
-				<TotalCO2 />
-				<CardGrid />
-			</Container>
-<CardUpdate />
-		</ChakraProvider>
-	);
+  return (
+    <ChakraProvider theme={theme}>
+      <Container w={'90vw'} paddingBlockEnd={10}>
+        <TotalCO2 />
+        <CardGrid />
+      </Container>
+      <CardUpdate />
+    </ChakraProvider>
+  );
 }
 
 export default App;
