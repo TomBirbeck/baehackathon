c9b853894809e133612c8c3d87c3e6118cecd94c
TomBirbeck <tombirbeck47@gmail.com>
2023-01-25 14:11:04 +0000

    working on card

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -1,32 +1,45 @@
-import { Button, Card, CardBody, CardFooter, CardHeader, Heading, SimpleGrid, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Heading,
+  SimpleGrid,
+  Text,
+} from '@chakra-ui/react';
+import TotalCO2 from './TotalCO2';
 
 const CardGrid = () => {
-	return (
-		<SimpleGrid spacing={4} templateColumns="repeat(auto-fill, minmax(200px, 1fr))">
-			<Card>
-				<CardHeader>
-					<Heading size="md"> Customer dashboard</Heading>
-				</CardHeader>
-				<CardBody>
-					<Text>View a summary of all your customers over the last month.</Text>
-				</CardBody>
-				<CardFooter>
-					<Button>View here</Button>
-				</CardFooter>
-			</Card>
-			<Card>
-				<CardHeader>
-					<Heading size="md"> Customer dashboard</Heading>
-				</CardHeader>
-				<CardBody>
-					<Text>View a summary of all your customers over the last month.</Text>
-				</CardBody>
-				<CardFooter>
-					<Button>View here</Button>
-				</CardFooter>
-			</Card>
-		</SimpleGrid>
-	);
+  return (
+    <SimpleGrid
+      spacing={4}
+      templateColumns='repeat(auto-fill, minmax(200px, 1fr))'
+    >
+      <Card borderWidth={2} borderColor='blackAlpha.900'>
+        <CardHeader display='grid' placeItems='center'>
+          <TotalCO2 />
+          <Heading size='md'>Network/API</Heading>
+          <Text size='sm'>Subtitle</Text>
+        </CardHeader>
+        <CardBody></CardBody>
+        <CardFooter>
+          <Button>View here</Button>
+        </CardFooter>
+      </Card>
+      <Card>
+        <CardHeader>
+          <Heading size='md'> Customer dashboard</Heading>
+        </CardHeader>
+        <CardBody>
+          <Text>View a summary of all your customers over the last month.</Text>
+        </CardBody>
+        <CardFooter>
+          <Button>View here</Button>
+        </CardFooter>
+      </Card>
+    </SimpleGrid>
+  );
 };
 
 export default CardGrid;
